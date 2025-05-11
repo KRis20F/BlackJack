f9dbe237a6c61cf91c3cc13dd388ec3f93dfb0df
alvardroid <alvardroid@gmail.com>
2025-05-11 09:48:14 +0200

    Sprite Changes + Api Removed

--- BlackJackApi/Models/User.cs
+++
@@ -1,9 +0,0 @@
-  namespace BlackJackApi.Models;
-  
-  public class User
-  {
-      public long Id { get; set; }
-      public string? Username { get; set; }
-      public string? Password { get; set; }
-      public int Cash { get; set; }
-  }
